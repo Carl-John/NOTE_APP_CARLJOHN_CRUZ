4446173ca68523d44a193b197f03a34ec760f98b
Carl_John <jy081320@gmail.com>
2022-10-21 14:17:32 -0700

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const fs = require('fs')
+const add = require('./add')
+const read = require('./read')
+const present = require('./present')
+// get user input
+const cmd = process.argv
 
-fs.writeFileSync('note.txt','Hello Welcome to Node.js')
+var note = {}
+
+if(cmd[2] == 'add') {
+    // Build Object
+    note = { 
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5]
+    }
+    // Get old note value
+    var oldNote = read()
+    // Add note to note.txt
+    add(note, oldNote)
+}
+
+if(cmd[2] == 'read') {
+    present(read())
+}
